cfc7a17d534a5adc3b618aeae38d261088b08012
Ilya.Gordeev <gordeevilyas@yandex.ru>
2023-10-06 11:53:43 +0300

    123

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,16 +1,18 @@
 <template>
   <div>
-    <img @click="this.$router.push('second')" style="width: 414px; height: 896px" :src="first" alt="">
+    <img @click="this.$router.push('second')" style="width: 414px; height: 896px" :src="firstMain" alt="">
+<!--    <img  :src="first" alt="">-->
   </div>
 </template>
 
 <script>
-import first from '@/assets/1-1.jpg'
+// import first from '@/assets/1-1.jpg'
+import firstMain from '@/assets/mainFirst.jpg'
 export default {
     name: 'IndexPage',
     data () {
       return {
-        first
+        firstMain
       }
     }
 }
